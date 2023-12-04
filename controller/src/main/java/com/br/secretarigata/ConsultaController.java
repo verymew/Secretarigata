2d7d5bfcd05f210f36d30d5793de9c24cdeb1929
Verymew <111842541+verymew@users.noreply.github.com>
2023-12-04 18:36:58 -0300

    Controller adicionado

---
+++ controller/src/main/java/com/br/secretarigata/ConsultaController.java
@@ -0,0 +1,19 @@
+package com.br.secretarigata;
+
+import com.br.dentuda.models.dao.ConsultasDao;
+import com.br.dentuda.models.dao.UsuarioDao;
+
+public class ConsultaController {
+    private UsuarioDao daousuario;
+    private ConsultasDao daoconsulta;
+    public ConsultaController(){ }
+    public void registrarConsulta(){
+
+    }
+    public void excluirConsulta(){
+
+    }
+    public void visualizarConsultas(){
+
+    }
+}
