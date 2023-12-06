8790e7f15078aa159a60edc3b2f99734a636db8d
Verymew <111842541+verymew@users.noreply.github.com>
2023-12-06 19:49:47 -0300

    Tirar projeto pai

--- view/src/main/java/com/br/secretarigata/view/controller/PaginaPrincipalController.java
+++ view/src/main/java/com/br/secretarigata/view/controller/PaginaPrincipalController.java
@@ -2,13 +2,25 @@ package com.br.secretarigata.view.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class PaginaPrincipalController {
 
     @FXML
-    private Label welcomeText;
+    private Button cadastrarcliente;
 
+    @FXML
+    private Button marcarconsulta;
+
+    @FXML
+    private Button visualizarconsulta;
+    @FXML
+    private Label welcomeText;
+    @FXML
+    void abrircliente(ActionEvent event) {
+
+    }
     @FXML
     void onHelloButtonClick(ActionEvent event) {
 
