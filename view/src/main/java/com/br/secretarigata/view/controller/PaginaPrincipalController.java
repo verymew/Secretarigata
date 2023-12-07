3e700344b31b4655bb3a469d3771bee09e4ff77c
Verymew <111842541+verymew@users.noreply.github.com>
2023-12-07 05:59:17 -0300

    Views ajeitadas

--- view/src/main/java/com/br/secretarigata/view/controller/PaginaPrincipalController.java
+++ view/src/main/java/com/br/secretarigata/view/controller/PaginaPrincipalController.java
@@ -1,9 +1,18 @@
 package com.br.secretarigata.view.controller;
 
+import com.br.secretarigata.view.PaginaPrincipal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PaginaPrincipalController {
 
@@ -14,16 +23,39 @@ public class PaginaPrincipalController {
     private Button marcarconsulta;
 
     @FXML
+    private AnchorPane principal;
+
+    @FXML
     private Button visualizarconsulta;
+
     @FXML
     private Label welcomeText;
-    @FXML
-    void abrircliente(ActionEvent event) {
 
+    @FXML
+    void mudarparacliente(ActionEvent event) throws IOException {
+        FXMLLoader fx = new FXMLLoader(PaginaPrincipalController.class.getResource("/com/br/secretarigata/view/pagina-criar-consulta.fxml"));
+        Scene s = new Scene(fx.load());
+        Stage st = new Stage();
+        st.setTitle("Cadastrar cliente");
+        st.setScene(s);
+        st.show();
+    }
+
+    @FXML
+    void visualizaremover(ActionEvent event) throws IOException {
+        FXMLLoader fx = new FXMLLoader(PaginaPrincipalController.class.getResource("/com/br/secretarigata/view/remover-visualizar-consultas.fxml"));
+        Scene s = new Scene(fx.load());
+        Stage st = new Stage();
+        st.setTitle("Visualizar e remover consultas");
+        st.setScene(s);
+        st.show();
     }
     @FXML
-    void onHelloButtonClick(ActionEvent event) {
-
-    }
-
-}
+    void marcaconsultas(ActionEvent event) throws IOException {
+        FXMLLoader fx = new FXMLLoader(PaginaPrincipalController.class.getResource("/com/br/secretarigata/view/visualizar-consultas.fxml"));
+        Scene s = new Scene(fx.load());
+        Stage st = new Stage();
+        st.setTitle("Marcar consulta");
+        st.setScene(s);
+        st.show();
+    }}
