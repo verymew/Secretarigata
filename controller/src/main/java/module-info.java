46c7472fc7520687603eb6bbb6dd3dd2b583930c
Verymew <111842541+verymew@users.noreply.github.com>
2023-12-06 22:51:49 -0300

    Erros consertado

---
+++ controller/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module controller {
+    requires model;
+
+    exports com.br.secretarigata.controllers;
+    exports com.br.secretarigata.controllers.dto;
+}
--- model/src/main/java/com/br/secretarigata/Main.java
+++ model/src/main/java/com/br/secretarigata/Main.java
@@ -7,15 +7,6 @@ import com.br.secretarigata.models.dao.UsuarioDao;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws Exception {
-        UsuarioDao dao = new UsuarioDao();
-        Usuario novo = new Usuario();
-        novo.setNome("mula");
-        novo.setCpf("2554758425");
-        novo.setEndereco("bonpland, 1047");
-
-        dao.salvar(novo);
-
-
 
     }
 
